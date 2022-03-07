196c764872644fb55772d6915a894eab7c7052eb
Nikita Paramonov <nparamonov27@gmail.com>
2022-03-07 18:26:51 +0300

    Update INN search by website address
    Added handling of multiple results returned by the database
    Returns a list of INNs only in case of a complete match with the website address

--- modules/class_SpecificParsing.py
+++ modules/class_SpecificParsing.py
@@ -4,25 +4,35 @@ from .class_Logs import logger
 
 
 class SpecificParsing(Browser):
-    def __init__(self):
-        super().__init__()
-        self.general_url_spark = 'https://spark-interfax.ru/search?Query='
 
+    # Проверка, содержится ли домен site в списке urls
+    @staticmethod
+    def __is_right_url(urls, site):
+        for temp in urls:
+            if temp.text in (site, 'www.' + site):
+                return True
+        return False
+
+    # Поиск ИНН по адресу сайта в базе данных spark-interfax.ru
     def find_inn_by_url(self, site):
-        url = self.general_url_spark + site
+        general_url = 'https://spark-interfax.ru/search?Query='
+        inn = []
+        url = general_url + site
         self.get(url)
         list_items = self.html.find_all('li', class_='search-result-list__item')
         if len(list_items) == 0:
-            return 0
-        elif len(list_items) > 1:
-            print('WARN: multiple results (', len(list_items), ')')
-            item = list_items[0]            # TODO: Выбрать из нескольких результатов один
+            logger.FAIL('INN not found in database spark-interfax')
+            return inn
         else:
-            item = list_items[0]
-        data = item.find('div', class_='code').text.split()
-        i = 0
-        while i < len(data):
-            if data[i] == 'ИНН':
-                return data[i+1]
-            i += 1
-        return 0
+            for item in list_items:
+                urls = item.find_all('span', class_='highlight')
+                if self.__is_right_url(urls, site):
+                    data = item.find('div', class_='code').text.split()
+                    i = 0
+                    while i < len(data):
+                        if data[i] == 'ИНН':
+                            inn.append(data[i + 1])
+                        i += 1
+                else:
+                    logger.WARN('Wrong site', item.find('span', class_='highlight').text)    # debug
+        return inn
